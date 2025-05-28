a300e1cc9d82bc5272cc8d739475ccba75e397e1
meganrobin <156228985+meganrobin@users.noreply.github.com>
2025-05-27 17:20:56 -0700

    Initial enemy behavior

---
+++ Scripts/Enemies/enemy_spawner.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@export var spawn_radius: float = 400 # Radius around the player
+@onready var parent = get_parent()
+@onready var player = parent.get_node_or_null("Player")
+@onready var enemy_container
+
+var EnemyClass = preload("res://Scenes/Enemies/Enemy.tscn")
+
+func _ready() -> void:
+	if GameManager.instance:
+		GameManager.instance.phase_changed.connect(_on_phase_changed)
+
+func start_wave() -> void:
+	print("WAVE STARTED")
+	# Create a dedicated container for enemies
+	enemy_container = Node2D.new()
+	enemy_container.name = "Enemies"
+	parent.add_child(enemy_container)
+	
+	$Timer.start()
+
+func end_wave() -> void:
+	print("WAVE ENDED")
+	enemy_container.queue_free()
+	$Timer.stop()
+
+# Handle phase changes
+func _on_phase_changed(new_phase: GameManager.Phase) -> void:
+	match new_phase:
+		GameManager.Phase.FIGHT:
+			start_wave()
+		GameManager.Phase.BUILD:
+			end_wave()
+
+func _on_timer_timeout() -> void:
+	if player: # Spawn an enemy within the spawn_radius if player was set correctly
+		var enemy = EnemyClass.instantiate()
+		var angle = randf_range(0,2*PI)
+		var spawn_position = player.global_position + Vector2.RIGHT.rotated(angle) * spawn_radius
+		enemy.global_position = spawn_position
+		enemy_container.add_child(enemy)
+	
